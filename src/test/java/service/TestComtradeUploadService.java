3d5a27d231cc4c0243b15f7428247c8b66419a20
Vhodnoylogin <kill.spama@gmail.com>
2023-07-16 17:36:03 +0300

    перенастроил логирование

--- src/test/java/service/TestComtradeUploadService.java
+++ src/test/java/service/TestComtradeUploadService.java
@@ -1,23 +1,34 @@
 package service;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.mpei.latushkina.fqw.FqwApplicationWOServer;
+import ru.mpei.latushkina.fqw.model.dto.point.ChartPoint;
 import ru.mpei.latushkina.fqw.service.ComtradeUploadService;
 
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = FqwApplicationWOServer.class)
 public class TestComtradeUploadService {
     @Autowired
     private ComtradeUploadService comtradeUploadService;
 
-
     @Test
     public void testYourService() {
-        comtradeUploadService.upload(null, null);
+        var res = comtradeUploadService.upload(null, null);
+
+        log.info("{}", res.size());
+        for (ChartPoint chartPoint : res) {
+            log.info("{}", chartPoint.getPoints().size());
+        }
+        for (ChartPoint chartPoint : res) {
+            log.info("{}", chartPoint.getPoints());
+        }
+
     }
 }
